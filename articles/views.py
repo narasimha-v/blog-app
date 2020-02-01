60f66c960637fe61051b7e9225ef9dea21d5cf81
Why-just-why <54150780+Why-just-why@users.noreply.github.com>
2020-02-01 23:44:42 +0530

    commit

--- articles/views.py
+++ articles/views.py
@@ -1,21 +1,24 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .models import Article
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from . import forms
 
+
 def articles(request):
     articles = Article.objects.all().order_by('date')
-    return render (request,'articles/articles_list.html',{'articles':articles})
+    return render(request, 'articles/articles_list.html', {'articles': articles})
 
-def article_details(request,slug):
+
+def article_details(request, slug):
     article = Article.objects.get(slug=slug)
-    return render(request,'articles/article_details.html',{'article':article})
+    return render(request, 'articles/article_details.html', {'article': article})
+
 
 @login_required
 def article_create(request):
     if request.method == 'POST':
-        form = forms.CreateArticle(request.POST,request.FILES)
+        form = forms.CreateArticle(request.POST, request.FILES)
         if form.is_valid():
             instance = form.save(commit=False)
             instance.author = request.user
@@ -23,4 +26,4 @@ def article_create(request):
             return redirect('articles:article-list')
     else:
         form = forms.CreateArticle()
-    return render(request,'articles/article_create.html',{'form':form})
+    return render(request, 'articles/article_create.html', {'form': form})
